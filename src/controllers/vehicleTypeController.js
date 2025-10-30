ad6c12978c230673514b77b323a84f75c198c841
Fredy López <133995369+fredylopez01@users.noreply.github.com>
2025-10-30 11:14:43 -0500

    feat: Add update and delete vehicle type endpoints

--- src/controllers/vehicleTypeController.js
+++ src/controllers/vehicleTypeController.js
@@ -1,8 +1,10 @@
 import { success } from "zod";
 import {
   createVehicleType,
+  deleteVehicleType,
   getAllVehicleTypes,
   getVehicleTypesByBranch,
+  updateVehicleType,
 } from "../services/vehicleTypeService.js";
 
 async function createVehicleTypeController(req, res, next) {
@@ -45,8 +47,38 @@ async function getVehicleTypesByBranchController(req, res, next) {
   }
 }
 
+async function updateVehicleTypeController(req, res, next) {
+  try {
+    const { id } = req.params;
+    const updateData = req.body;
+    const updatedVehicleType = await updateVehicleType(Number(id), updateData);
+    return res.status(200).json({
+      success: true,
+      message: "Tipo de vehículo actualizado correctamente.",
+      data: updatedVehicleType,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function deleteVehicleTypeController(req, res, next) {
+  try {
+    const { id } = req.params;
+    await deleteVehicleType(Number(id));
+    return res.status(200).json({
+      success: true,
+      message: "Tipo de vehículo eliminado correctamente.",
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
 export {
   createVehicleTypeController,
   getAllVehicleTypeController,
   getVehicleTypesByBranchController,
+  updateVehicleTypeController,
+  deleteVehicleTypeController,
 };
